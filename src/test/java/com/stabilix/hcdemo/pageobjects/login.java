3ab103e1f3ead9733fdfdd8ae9c861c1db3d27d4
arun <arun@STXLAPTOP01.stabilixindia.biz>
2018-10-31 18:09:14 +0530

    new comment

--- src/test/java/com/stabilix/hcdemo/pageobjects/login.java
+++ src/test/java/com/stabilix/hcdemo/pageobjects/login.java
@@ -42,7 +42,7 @@ public class login extends commoncode{
 	@CacheLookup
 	public WebElement password; 
 	
-	//test
+	//test is the change
 	@FindBy(how=How.ID,using= "felix-widget-button-btnSignIn")
 	@CacheLookup
 	public WebElement submit;
